69279a986612de411f3f816e0d2cae064c5f9825
JanTheEnd <endriusza@interia.pl>
2018-08-14 18:38:58 +0200

    Lerning Python

--- zen_pythona.py
+++ zen_pythona.py
@@ -1,6 +1,6 @@
 #import this
 
-"""Endriusz_____Beautiful is better than ugly.
+"""Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.
