311c21e0bc9322857fb1855fab34d8dd2a55caed
EndTheEnd <endi1207@gmail.com>
2018-08-15 13:23:16 +0200

    funkcje

--- Funkcje/funkcje.py
+++ Funkcje/funkcje.py
@@ -71,3 +71,5 @@ def get_formatted_name(first_name, last_name, middle_name=''):
 musician = get_formatted_name('jimi','hendrix')
 print(musician)
 musician = get_formatted_name('john', 'hooker', 'lee')
+
+########## zwrot słownika
