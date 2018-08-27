dcb13df8809e04ee81a658c479fdf05cf33e5c06
EndTheEnd <endi1207@gmail.com>
2018-08-27 11:45:12 +0200

    funkcje

--- Funkcje/funkcje.py
+++ Funkcje/funkcje.py
@@ -61,14 +61,60 @@
 # musician = get_formatted_name('john', 'lee', 'hooker')
 # print(musician)
 
-def get_formatted_name(first_name, last_name, middle_name=''):
-    """Zwraca elegancko sformatowane pełne imię i nazwisko."""
-    if middle_name:
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
-    else:
-        full_name = first_name + ' ' + last_name
+# def get_formatted_name(first_name, last_name, middle_name=''):
+#     """Zwraca elegancko sformatowane pełne imię i nazwisko."""
+#     if middle_name:
+#         full_name = first_name + ' ' + middle_name + ' ' + last_name
+#     else:
+#         full_name = first_name + ' ' + last_name
+#     return full_name.title()
+# musician = get_formatted_name('jimi','hendrix')
+# print(musician)
+# musician = get_formatted_name('john', 'hooker', 'lee')
+# print(musician)
+
+########## zwrot słownika
+# def build_person(first_name, last_name, age=''):
+#     """Zwraca słownik informacji o danej osobie"""
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+# musician = build_person('jimi', 'hendrix', age=27)
+# print(musician)
+
+########## używanie funkcji wraz petla while
+# def get_formatted_name(first_name, last_name):
+#     """Zwraca elegancko sformatowne pełne imie i nazwisko."""
+#     full_name = first_name + ' ' + last_name
+#     return full_name.title()
+# To jest petla działąca w nieksoczoność!
+# while True:
+#     print("\nProsze podac imie i nazwisko:")
+#     f_name = input("Imię: ")
+#     l_name = input("Nazwisko: ")
+
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print("\nWitaj, " + formatted_name + "!")
+
+
+def get_formatted_name(first_name, last_name):
+    """Zwraca elegancko sformatowne pełne imie i nazwisko."""
+    full_name = first_name + ' ' + last_name
     return full_name.title()
-musician = get_formatted_name('jimi','hendrix')
-print(musician)
-musician = get_formatted_name('john', 'hooker', 'lee')########## zwrot słownika
-print(musician)
+
+# To jest petla działąca w nieksoczoność!
+while True:
+    print("\nProsze podac imie i nazwisko:")
+    print("(wpisz 'q', aby zakonczyć prace w dowolnym momencie)")
+
+    f_name = input("Imię: ")
+    if f_name == 'q':
+        break
+    
+    l_name = input("Nazwisko:")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nWitaj, " + formatted_name + "!")
